076fcd7bd2d17a047c82ca67b8e1b6eb647b3ff7
Macm91 <macm91.mc@gmail.com>
2021-12-08 15:43:59 -0500

    completed problem 2. capitalize each word in a phrase function

--- ProblemSolving1.js
+++ ProblemSolving1.js
@@ -18,3 +18,23 @@ reverseString("hello")
 //a.	Write code that takes a string as input and capitalize the first letter of each word. 
 //Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
 
+let finalPhrase = '';
+
+function capitalize(x){
+    for (let i = 0; i<x.length; i++){
+        if (i==0){
+            finalPhrase += (x[i].toUpperCase())
+        }
+        else if (x[i] === ' '){
+            finalPhrase += x[i];
+            i++
+            finalPhrase += (x[i].toUpperCase())
+        }
+        else{
+            finalPhrase += x[i];
+        }
+    }
+    console.log(finalPhrase)
+}
+
+capitalize("hello world")
