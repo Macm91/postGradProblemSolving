f27f094dfbc7fd5174d99710d2b8ac217ae4bb69
Macm91 <macm91.mc@gmail.com>
2021-12-08 15:22:10 -0500

    redid problem 1 to eliminate the reversed input coming back as an array. Now it comes back as a string

--- ProblemSolving1.js
+++ ProblemSolving1.js
@@ -1,18 +1,20 @@
 "use strict";
 
 // 1.	Reverse a string
-// a.	Write code that takes a string as input and returns the string reversed
-// b.	i.e. “Hello” will be returned as “olleH”
 
+let output = ' ';
 
-let word = "Hello";
-let reverse = [];
-
-function reverseString(x){
-    for (let i=x.length-1; i>=0 ; i--){
-        reverse.push(x[i])
+function reverseString(str){
+    for (let i=str.length-1; i>=0 ; i--){
+       output += str[i]
     }
+    console.log(output)
+    
 }
 
-reverseString(word);
-console.log(reverse)
+reverseString("hello")
+
+// 2. 2.	Capitalize letter
+//a.	Write code that takes a string as input and capitalize the first letter of each word. 
+//Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
+
