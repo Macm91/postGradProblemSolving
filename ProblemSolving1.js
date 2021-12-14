b1875e0a391d811de72908837d32255ceadc95af
Macm91 <macm91.mc@gmail.com>
2021-12-14 10:28:06 -0500

    finished compress a string problem

--- ProblemSolving1.js
+++ ProblemSolving1.js
@@ -46,22 +46,32 @@ capitalize("test test")
 //For example, an input of "aaabbbbbccccaacccbbbaaabbbaaa"
 // would compress to "3a5b4c2a3c3b3a3b3a"
 
-// function compress(x){
-//     let compressed = '';
-    
-//     for (let i = 0; i<x.length; i++){
-//         let count = 1
-//         for(let j = 0; j<x.length; j++ ){
-//             if (x[i] === x[j]){
-//                 count++
-//             }
-            
-//         }
-//     }
+function compress(x){
+    let compressed = '';
+    let count = 0
+    debugger
+    for (let i = 0; i<x.length; i++){
+        for(let j = 0; j<x.length; j++ ){
+            if (x[i] === x[j]){
+                count++
+            }
+        }
+        compressed+=(count + x[i])
+        i +=(count-1)
+        count = 0
+        
+    }
+    console.log(compressed)
+}
 
-// }
+compress("aaabbbcdd")
 
-//Array subsets problem. 
+
+
+
+
+
+// EXTRA PROBLEM: Array subsets problem. 
 
 let array = [1,2,3,2,5,6];
 
@@ -78,7 +88,6 @@ function sum(arr){
 
 
 function subsetA(arr) {
-    debugger
     for ( let i=0; i<arr.length; i++){
         for (let j=1; j<arr.length; j++){
             if (i != j){
@@ -87,7 +96,7 @@ function subsetA(arr) {
             let sumOfArrA = arr[i] + arr[j]
                 if (sumOfArrA > arrB){
                     let arrA = [arr[i], arr[j]]
-                    return (arrA)
+                    console.log (arrA)
                 }
         }
     }
@@ -95,3 +104,9 @@ function subsetA(arr) {
 }
 
 subsetA(array)
+
+
+
+//write a Degree of an Array where you intake an array. 
+//in the array you find the most frequent numbers (deg. of array)
+//find length of the shortest sub array that shares that degree. 
